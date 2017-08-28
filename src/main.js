89dadcb202871aedcd905ee8c8b6e99f712d094b
bigWave <chenlong8520@foxmail.com>
2017-08-28 11:42:21 +0800

    不使用自动设置html的fontsize

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import GlobalComponents from './components';
-import './utils/html.rem.js';
 
 Vue.config.productionTip = false;
 
